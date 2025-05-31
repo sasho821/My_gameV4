7deee19b69dcdd2903f5472d69cf9724a119ab84
glintcher <aaakhmeev@yandex.ru>
2025-05-31 14:22:53 +0300

    добавил элементы управления на форму

--- UI/ui_window.py
+++ UI/ui_window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import (QWidget, QMainWindow, QDesktopWidget,
-QLineEdit, QGridLayout, QVBoxLayout)
+QLineEdit, QGridLayout, QVBoxLayout, QCheckBox, QPushButton)
 from PyQt5.QtCore import Qt
 
 class startForm(QMainWindow):
@@ -25,8 +25,13 @@ class startForm(QMainWindow):
                 background: transparent;
             }
         """)
-        self.main_layout.addLayout(self.grid)
+
+        self.add_team_inp_field()
+
+        self.main_layout.setLayout(self.grid)
         self.setCentralWidget(self.main_layout)
+        # События
+        self.exit_btn.clicked.connect(self.close)
 
     def center(self):
         # Получаем геометрию доступной части экрана
@@ -48,10 +53,37 @@ class startForm(QMainWindow):
     # Добавление в основную разметку полей ввода
     def add_team_inp_field(self):
         self.vbox = QVBoxLayout()
+        self.setingsvbox = QVBoxLayout()
         
+        # Добавляю поля с названиями команд
         for i in range(5):
             team_inp = QLineEdit()
             team_inp.setObjectName("team_inp_field_{}".format(i))
-            self.vbox.addWidget(team_inp)
-            
-        self.grid.addLayout(self.vbox, 0, 1)
+            self.vbox.addWidget(team_inp, alignment=Qt.AlignTop)
+        
+        # Кнопка "старт"
+        self.start_btn = QPushButton("Начать")
+        # Кнопка "Выход"
+        self.exit_btn = QPushButton("Выход")
+
+        # Добавляю настройки
+        self.random_queue_box = QCheckBox("Случайная очередность")
+        self.cat_in_bag_box = QCheckBox("Кот в мешке")
+        self.mines_box = QCheckBox("Мины")
+        self.change_game_btn = QPushButton("Выбрать тему")
+        # Размещаю настройки на форме
+        self.setingsvbox.addWidget(self.change_game_btn)
+        self.setingsvbox.addWidget(self.random_queue_box)
+        self.setingsvbox.addWidget(self.cat_in_bag_box)
+        self.setingsvbox.addWidget(self.mines_box)
+        #Размещаю кнопки на форме
+        self.vbox.addWidget(self.start_btn)
+        self.vbox.addWidget(self.exit_btn)
+        
+        self.grid.addLayout(self.vbox, 0, 2)
+        self.grid.addLayout(self.setingsvbox,0,1)
+
+        self.grid.setColumnStretch(1, 2)
+        self.grid.setColumnStretch(2, 1)
+
+    

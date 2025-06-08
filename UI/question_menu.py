07b8448561eee08f4d4df862399ed005d79bf6db
glintcher <aaakhmeev@yandex.ru>
2025-06-08 10:26:09 +0300

    Добавил название категорий

--- UI/question_menu.py
+++ UI/question_menu.py
@@ -11,10 +11,29 @@ class QuestionMenuWindow(QMainWindow):
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
 
-        # Создаем GridLayout
+        # Создаем GridLayout - общий
         grid_layout = QGridLayout()
+        # Создаем GridLayout - для вопросов
+        quest_grid_layout = QGridLayout()
 
         # Создаю элементы
+        self.theme_name_lbl = QLabel("Название темы")
+
+        # Размещаю элементы в сетке вопросов
+        # 1. Размещаю название категорий
+        for i in range(5):
+            buf_lbl = QLabel("Категория_{}".format(i))
+            buf_lbl.setObjectName("cat_name_{}".format(i))
+            quest_grid_layout.addWidget(buf_lbl, (i + 1), 1, alignment=Qt.AlignCenter)
+
+        # Размещаю элементы в общей сетке
+        grid_layout.addWidget(self.theme_name_lbl, 1, 1, 1, 2, alignment=Qt.AlignCenter)
+        grid_layout.addLayout(quest_grid_layout, 2, 1, alignment=Qt.AlignCenter)
+
+        # Настройка общей сетки
+        # Настройка растяжения строчек
+        grid_layout.setRowStretch(1, 1) 
+        grid_layout.setRowStretch(2, 5)
 
         centralWidget.setLayout(grid_layout)
 
